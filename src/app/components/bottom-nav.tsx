43df36ae047cdc3b6efd31955e3cc57a89e7a5aa
ervnjmsdnts <earvinjamesdantes@gmail.com>
2024-03-21 19:17:33 +0800

    Added dots on all nav items and added transaction history and settings page

--- src/app/components/bottom-nav.tsx
+++ src/app/components/bottom-nav.tsx
@@ -2,6 +2,7 @@
 
 import { cn } from '@/lib/utils';
 import { Home, Settings, Wallet } from 'lucide-react';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default function BottomNav() {
@@ -10,16 +11,37 @@ export default function BottomNav() {
   return (
     <nav className='h-16 bg-primary rounded-t-xl px-16'>
       <div className='flex h-full items-center text-primary-foreground justify-between'>
-        <div>
+        <Link href='/'>
           <Home className={cn('h-8 w-8', pathname === '/' && 'text-white')} />
           {pathname === '/' && (
             <div className='flex items-center justify-center relative'>
               <div className='w-1 h-1 bg-white rounded-full absolute top-1' />
             </div>
           )}
-        </div>
-        <Wallet className='h-8 w-8' />
-        <Settings className='h-8 w-8' />
+        </Link>
+        <Link href='/transaction-history'>
+          <Wallet
+            className={cn(
+              'h-8 w-8',
+              pathname === '/transaction-history' && 'text-white',
+            )}
+          />
+          {pathname === '/transaction-history' && (
+            <div className='flex items-center justify-center relative'>
+              <div className='w-1 h-1 bg-white rounded-full absolute top-1' />
+            </div>
+          )}
+        </Link>
+        <Link href='/settings'>
+          <Settings
+            className={cn('h-8 w-8', pathname === '/settings' && 'text-white')}
+          />
+          {pathname === '/settings' && (
+            <div className='flex items-center justify-center relative'>
+              <div className='w-1 h-1 bg-white rounded-full absolute top-1' />
+            </div>
+          )}
+        </Link>
       </div>
     </nav>
   );
